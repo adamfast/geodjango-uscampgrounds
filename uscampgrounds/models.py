0bf936bdd5ad7a495f997c1162ac1a2c4efa9903
Adam Fast <adam@adamfast.com>
2011-11-03 18:23:58 -0500

    The free-form data is often longer than 128 characters. Warning: this is not backwards-compatible, you will need to do manage.py reset on the app and re-import the latest files from uscampgrounds.info.

--- uscampgrounds/models.py
+++ uscampgrounds/models.py
@@ -3,13 +3,13 @@ from django.contrib.gis.db import models
 
 class Campground(models.Model):
     campground_code = models.CharField(max_length=64)
-    name = models.CharField(max_length=128)
-    campground_type = models.CharField(max_length=128)
-    phone = models.CharField(max_length=128)
+    name = models.CharField(max_length=256)
+    campground_type = models.CharField(max_length=256)
+    phone = models.CharField(max_length=256)
     comments = models.TextField()
-    sites = models.CharField(max_length=128)
-    elevation = models.CharField(max_length=128)
-    hookups = models.CharField(max_length=128)
+    sites = models.CharField(max_length=256)
+    elevation = models.CharField(max_length=256)
+    hookups = models.CharField(max_length=256)
     amenities = models.TextField()
     point = models.PointField(srid=4326)
 
