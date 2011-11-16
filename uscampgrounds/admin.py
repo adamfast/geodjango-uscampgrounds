143b74a2c6f99d2d92ac85310351327ffb630c1e
Adam Fast <adam@adamfast.com>
2011-11-15 20:02:41 -0600

    Allow searching campgrounds by name for convenience.

--- uscampgrounds/admin.py
+++ uscampgrounds/admin.py
@@ -4,5 +4,6 @@ from uscampgrounds.models import *
 class CampgroundAdmin(admin.OSMGeoAdmin):
     list_display = ('name', 'campground_code', 'campground_type', 'phone', 'sites', 'elevation', 'hookups', 'amenities')
     list_filter = ('campground_type',)
+    search_fields = ('name',)
 
 admin.site.register(Campground, CampgroundAdmin)
