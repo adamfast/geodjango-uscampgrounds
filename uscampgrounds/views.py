fc4a3acb984c2c2172f3c6b1fcfed4465605ba30
Adam Fast <adam@adamfast.com>
2012-10-20 12:48:10 -0500

    Add logic to restrict down the camps shown.

--- uscampgrounds/views.py
+++ uscampgrounds/views.py
@@ -11,6 +11,14 @@ def nearby_campgrounds(request):
     else:
         camps = Campground.objects.all().order_by('name')
 
+    try:  # if it's a number, use it.
+        retrieve_count = int(request.GET.get('count', 20))
+    except ValueError:  # passed us something besides a number, default to something sane
+        retrieve_count = 20
+
+    if retrieve_count != -1:  # but if we got -1 give them all
+        camps = camps[:retrieve_count]
+
     return render_to_response('uscampgrounds/nearby.html', {
         'object_list': camps
     }, context_instance=RequestContext(request))
